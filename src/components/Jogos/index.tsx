0146e24619a1ff10e324a7e42b6a555e6208ce37
Gustavo Souza <gustavo.ifto@gmail.com>
2020-02-10 18:30:06 -0300

    Inserindo card de jogos

--- src/components/Jogos/index.tsx
+++ src/components/Jogos/index.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 
-import { Container, Text } from './styles';
+import './styles.css';
+import lol from '../../assets/lol.jpg';
 
 const Jogos: React.FC = () => {
   return(
-    <Container>
-      <Text>
-        Paginas de Jogos
-      </Text>
-    </Container>
+    <div className="container">
+
+      <div className="card middle">
+        <div className="front">
+          <img src={lol} alt="League of Legends"/>
+        </div>
+        <div className="back">
+          <div className="back-content middle">
+            <h2>League of Legends</h2>
+            <span>LOL</span>
+            <div className="button">
+              <button>inscrever-se</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
   );
 }
 
